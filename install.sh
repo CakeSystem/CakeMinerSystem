050e455a1e07437c25c9755e79d3df99c2cbe9e2
CakeSystem <1625000148@qq.com>
2025-03-13 09:17:14 +0800

    4.4.0

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="4.3.4"
+VERSION="4.4.0"
 DOWNLOAD_HOST="https://github.com/CakeSystem/CakeMinerSystem/raw/main/linux"
 ORIGIN_EXEC="CakeMinerSystem-${VERSION}"
 
@@ -389,6 +389,7 @@ ISSUE() {
 	echo "4.2.0"
 	echo "4.3.0"
     echo "4.3.4"
+	echo "4.4.0"
 }
 
 filterResult() {
